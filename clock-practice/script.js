ae72425adaee2911ee6ba45a135f1cdffcc0964e
LucasNemeth <l_orion_n@hotmail.com>
2020-07-11 18:39:39 -0400

    finished the clock. able to tell 24 hours and 12 hours. styled it

--- clock-practice/script.js
+++ clock-practice/script.js
@@ -6,14 +6,20 @@ function currentTime(){
     let hour = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
+    let midday = "AM";
+    midday = (hour >= 12) ? "PM" : "AM";
+
+    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
     day= updateTime(day);
     hour = updateTime(hour);
     min = updateTime(min);
     sec = updateTime(sec);
-    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec;
+    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec + " " + midday;
     let timeOut = setTimeout(function(){
         currentTime()
     }, 1000) ;
+
+ 
 }       
 
 function updateTime(k) {
