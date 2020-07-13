e2b3a38618c558e4b058deec85c66416f96ac9fe
LucasNemeth <l_orion_n@hotmail.com>
2020-07-13 15:52:47 -0400

    doing the click functions for the keys

--- calc-practice/script.js
+++ calc-practice/script.js
@@ -6,3 +6,31 @@
 // the clear key
 
 //we must listen for a key press and determine what kind of key was pressed
+
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelector('.calc-keys');
+
+keys.addEventListener('click', e =>{
+    if (e.target.matches('button')){
+        const key = e.target;
+        const action = key.dataset.action;
+        if (!action) {
+            console.log("hey are ya working?")
+        } else if(
+            action === "add" ||
+            action === "subtract" ||
+            action === "divide" ||
+            action === "multiply"
+        ){
+            console.log("I can do math")
+        } else if(action === "decimal"){
+            console.log("dot dot dot")
+        } else if (action === "calculate"){
+            console.log("the great equalizer")
+        } else if (action ==="clear"){
+            console.log("byebye")
+        }
+    }
+});
+
+
