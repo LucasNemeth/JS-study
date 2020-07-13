9f19dd03885a3bb6277595c3453453712421c694
LucasNemeth <l_orion_n@hotmail.com>
2020-07-12 21:30:01 -0400

    first commit for Calculator

---
+++ calc-practice/script.js
@@ -0,0 +1,8 @@
+//Stuff we need:
+// a number key(0–9)
+// an operator key(+, -, ×, ÷)
+// the decimal key
+// the equals key
+// the clear key
+
+//we must listen for a key press and determine what kind of key was pressed
