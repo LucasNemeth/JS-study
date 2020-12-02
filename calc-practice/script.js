ecf16b6dd6b9763b4d2bebe11818eda4ea81cd42
LucasNemeth <l_orion_n@hotmail.com>
2020-12-02 15:59:10 -0500

    refactoring code, able to display the numbers in input. getting the actual operators functionality down

--- calc-practice/script.js
+++ calc-practice/script.js
@@ -7,195 +7,146 @@
 
 //we must listen for a key press and determine what kind of key was pressed
 
-const calculator = document.querySelector('.calculator');
-const keys = document.querySelector('.calc-keys');
-const display = document.querySelector('.calc-display');
+// const calculator = document.querySelector('.calculator');
+// const keys = document.querySelector('.calc-keys');
+// const display = document.querySelector('.calc-display');
 
-keys.addEventListener('click', e =>{
-    if (e.target.matches('button')){
-        const key = e.target;
-        const action = key.dataset.action;
-        const keyContent =key.textContent;
-        const displayedNum = display.textContent;
-        const previousKeyType = calculator.dataset.previousKeyType;
+// keys.addEventListener('click', e =>{
+//     if (e.target.matches('button')){
+//         const key = e.target;
+//         const action = key.dataset.action;
+//         const keyContent =key.textContent;
+//         const displayedNum = display.textContent;
+//         const previousKeyType = calculator.dataset.previousKeyType;
         
 
-        if (!action) {
-            if (displayedNum === '0' || previousKeyType ==="operator") {
-                display.textContent = keyContent;
-            } else {
-                display.textContent = displayedNum + keyContent;
-            }
-            // console.log("hey are ya working?")
-        } if(
-            action === "add" ||
-            action === "subtract" ||
-            action === "divide" ||
-            action === "multiply"
-        )
+//         if (!action) {
+//             if (displayedNum === '0' || previousKeyType ==="operator") {
+//                 display.textContent = keyContent;
+//             } else {
+//                 display.textContent = displayedNum + keyContent;
+//             }
+//             // console.log("hey are ya working?")
+//         } if(
+//             action === "add" ||
+//             action === "subtract" ||
+//             action === "divide" ||
+//             action === "multiply"
+//         )
         
-        {
-            key.classList.add('is-depressed');
-            calculator.dataset.previousKeyType = 'operator';
-        //if user clicks the decimal
-        }
-        {
-            calculator.dataset.firstValue = displayedNum;
-            calculator.dataset.operator = action
-        }
-        if(action === "decimal"){
-            display.textContent = displayedNum + ".";
-        }  if (action === "calculate"){
-            const firstValue= calculator.dataset.firstValue;
-            const operator = calculator.dataset.operator;
-            const secondValue = displayedNum
+//         {
+//             key.classList.add('is-depressed');
+//             calculator.dataset.previousKeyType = 'operator';
+//         //if user clicks the decimal
+//         }
+//         {
+//             calculator.dataset.firstValue = displayedNum;
+//             calculator.dataset.operator = action
+//         }
+//         if(action === "decimal"){
+//             display.textContent = displayedNum + ".";
+//         }  if (action === "calculate"){
+//             const firstValue= calculator.dataset.firstValue;
+//             const operator = calculator.dataset.operator;
+//             const secondValue = displayedNum
 
             
 
-            const calculate = (n1, operator, n2) => {
-                let result = '';
+//             const calculate = (n1, operator, n2) => {
+//                 let result = '';
 
-                if (operator === 'add') {
-                    result = parseFloat(n1) + parseFloat(n2);
-                    console.log("hi", result)
-                } else if (operator === 'subtract') {
-                    result = parseFloat(n1) - parseFloat(n2);
-                } else if (operator === 'multiply') {
-                    result = parseFloat(n1) * parseFloat(n2);
-                } else if (operator === 'divide') {
-                    result = parseFloat(n1) / parseFloat(n2)
-                }
-                console.log(result)
-                return result
-            }
-            display.textContent = calculate(firstValue, operator, secondValue)
-        } if (action ==="clear"){
-            display.textContent = "0"
-            // console.log("byebye")
-        }
-        Array.from(key.parentNode.children)
-            .forEach(k => k.classList.remove(".is-depressed"));
+//                 if (operator === 'add') {
+//                     result = parseFloat(n1) + parseFloat(n2);
+//                     console.log("hi", result)
+//                 } else if (operator === 'subtract') {
+//                     result = parseFloat(n1) - parseFloat(n2);
+//                 } else if (operator === 'multiply') {
+//                     result = parseFloat(n1) * parseFloat(n2);
+//                 } else if (operator === 'divide') {
+//                     result = parseFloat(n1) / parseFloat(n2)
+//                 }
+//                 console.log(result)
+//                 return result
+//             }
+//             display.textContent = calculate(firstValue, operator, secondValue)
+//         } if (action ==="clear"){
+//             display.textContent = "0"
+//             // console.log("byebye")
+//         }
+//         Array.from(key.parentNode.children)
+//             .forEach(k => k.classList.remove(".is-depressed"));
         
-        // calculate()
+//         // calculate()
 
-        //having it so we replace the placeholder display number with the number keys we hit
-        // if(!action){
-        //     if (displayedNum === '0'){
-        //         display.textContent = keyContent;
-        //     } else {
-        //         display.textContent = displayedNum + keyContent;
-        //     }
-        // }
-    }
-});
+//         //having it so we replace the placeholder display number with the number keys we hit
+//         // if(!action){
+//         //     if (displayedNum === '0'){
+//         //         display.textContent = keyContent;
+//         //     } else {
+//         //         display.textContent = displayedNum + keyContent;
+//         //     }
+//         // }
+//     }
+// });
 
 //1. on click of number keys. show that number in the display
 // const display = document.querySelector('.calc-display');
 
 
 //Let's try another script, cuz this above one ain't really working
-// (function() {
-//     console.log("can you hear me?")
-//     "use strict";
+let input = document.querySelector('.calc-display'),
+    number = document.querySelectorAll(".num"),
+    operator = document.querySelectorAll(".operator"),
+    result = document.querySelector(".equal"),
+    clear = document.querySelector(".is-clear"),
+    // display = document.querySelector('.calc-display'),
+    resultDisplayed = false;
 
-//     //a way for us to get us elements quick
-//     let el = function(element) {
-//         if (element.charAt(0) === "#")
-//         { 
-//             //if it passed an ID
-//             return document.querySelector(element);
-//             //returns a single element
-//         } 
+for (let i = 0; i < number.length ; i++){
+    number[i].addEventListener("click", function(e){
+        // e.preventDefault();
+        //stores current input string and last character in variables
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
 
-//         return document.querySelectorAll(element);
-//         //otherwise,  returns the nodelist
-//     };
+        //if result isn't displaying, just keep adding
+        if(resultDisplayed === false){
+            console.log("1")
+            input.innerHTML += e.target.innerHTML;
+        } else if (resultDisplayed === true && lastChar === "+"  || lastChar === "-" || lastChar === "&times" || lastChar === "÷") {
+        //if result is currently displayed and pressed a number
+        //clear the input string and add the new input to start new operation
+            console.log("2")
+            resultDisplayed = false;
+            input.innerHTML += e.target.innerHTML;
+        } else {
+        //if result is currently displayed and user pressed a number
+        //we need to clear the input string and add the new input to start the new operation
+            console.log("3")
+            resultDisplayed = false;
+            input.innerHTML = "";
+            input.innerHTML += e.target.innerHTML;
+        }
+    });
+}
 
-//     const display = el(".display"),
-//     equals = el(".equal"),
-//     nums = el(".num"),
-//     ops= el(".operator"),
-//     theNum = "",
-//     oldNum = "",
-//     resultNum = "",
-//     operator = "";
+//click handlers to number buttons
+for(let i = 0; i < operator.length; i++){
+    operator[i].addEventListener("click", function(e){
 
-//     //when number is clicked, get the current number selected.
-//     const setNum = function(){
-//         if (resultNum){
-//             theNum = this.getAttribute("data-num");
-//             resultNum = "";
-//         } else {
-//             theNum += this.getAttribute("data-num");
-//         }
-//         display.innerHTML = theNum;
-//     };
-//     // when the operator is clicked, pass the number to oldNum  and save the operator
-//     const moveNum = function(){
-//         oldNum = theNum;
-//         theNum = "";
-//         operator = this.getAttribute("data-ops");
-//         equals.setAttribute("data-result", "");  // reset the result in attr
-//     };
+        //storing current input string and last char in var
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length - 1];
 
-//     const displayNum = function(){
-//         oldNum = parseFloat(oldNum);
-//         theNum = parseFloat(theNum);
-
-//         switch(operator) {
-//             case "plus": resultNum = oldNum + theNum;
-//             break;
-            
-//             case "minus": 
-//             resultNum = oldNum - theNum;
-//             break;
-
-//             case "times":
-//             resultNum = oldNum * theNum;
-//             break; 
-
-//             case "divided by":
-//             resultNum = oldNum / theNum;
-//             break;
-
-//             //if equal is pressed w/o an op, keep number and continue
-//             default:
-//                 resultNum = theNum;
-//         }
-//         //if NaN or infinity is returned
-//         if(!isFinite(resultNum)){
-//             if(isNaN(resultNum)){
-//                 resultNum = "you broke it, y'dangus!";
-//             } else {
-//                 //if result is infinity
-//                 resultNum="OH NOOOO";
-
-//                 // el('#calculator').classList.add("broken"); //break calculator
-
-//                 // el("#reset").classList.add("show");
-//             }
-//         }
-
-//         //now to display the results
-//         display.innerHTML = resultNum;
-//         equals.setAttribute('data-result', resultNum);
-
-//         oldNum = 0;
-//         theNum = resultNum;
-//     };
-//     const clearAll = function(){
-//         oldNum = "";
-//         theNum = "";
-//         display.innerHTML = "0";
-//         equals.setAttribute("data-result",resultNum);
-//     };
-
-//     //click evento!
-//     for(let i = 0, l = nums.length; i < l; i++){
-//         nums[i].onclick = setNum;
-//     }
-
-//     for (let i = 0, l = ops.length; i < l; i++){
-//         ops[i].onclick = moveNum;
-//     }
-// }());
+        //if last character entered is operator, replace with currently pressed one
+        if (lastChar === "+" || lastChar === "-" || lastChar === "&times" || lastChar === "÷") {
+            let newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML; 
+            input.innerHTML = newString;
+        } else if (currentString.length == 0){
+            // if first key pressed is an operator don't do anything
+            console.log("do something else dawg")
+        } else {
+            input.innerHTML += e.target.innerHTML
+        }
+    })
+}
