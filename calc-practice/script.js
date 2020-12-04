16354100b0d656b586435b324963a1ddbee07b8f
LucasNemeth <l_orion_n@hotmail.com>
2020-12-03 21:52:59 -0500

    got the operators done, wokring on decimal functionality

--- calc-practice/script.js
+++ calc-practice/script.js
@@ -1,93 +1,4 @@
-//Stuff we need:
-// a number key(0–9)
-// an operator key(+, -, ×, ÷)
-// the decimal key
-// the equals key
-// the clear key
 
-//we must listen for a key press and determine what kind of key was pressed
-
-// const calculator = document.querySelector('.calculator');
-// const keys = document.querySelector('.calc-keys');
-// const display = document.querySelector('.calc-display');
-
-// keys.addEventListener('click', e =>{
-//     if (e.target.matches('button')){
-//         const key = e.target;
-//         const action = key.dataset.action;
-//         const keyContent =key.textContent;
-//         const displayedNum = display.textContent;
-//         const previousKeyType = calculator.dataset.previousKeyType;
-        
-
-//         if (!action) {
-//             if (displayedNum === '0' || previousKeyType ==="operator") {
-//                 display.textContent = keyContent;
-//             } else {
-//                 display.textContent = displayedNum + keyContent;
-//             }
-//             // console.log("hey are ya working?")
-//         } if(
-//             action === "add" ||
-//             action === "subtract" ||
-//             action === "divide" ||
-//             action === "multiply"
-//         )
-        
-//         {
-//             key.classList.add('is-depressed');
-//             calculator.dataset.previousKeyType = 'operator';
-//         //if user clicks the decimal
-//         }
-//         {
-//             calculator.dataset.firstValue = displayedNum;
-//             calculator.dataset.operator = action
-//         }
-//         if(action === "decimal"){
-//             display.textContent = displayedNum + ".";
-//         }  if (action === "calculate"){
-//             const firstValue= calculator.dataset.firstValue;
-//             const operator = calculator.dataset.operator;
-//             const secondValue = displayedNum
-
-            
-
-//             const calculate = (n1, operator, n2) => {
-//                 let result = '';
-
-//                 if (operator === 'add') {
-//                     result = parseFloat(n1) + parseFloat(n2);
-//                     console.log("hi", result)
-//                 } else if (operator === 'subtract') {
-//                     result = parseFloat(n1) - parseFloat(n2);
-//                 } else if (operator === 'multiply') {
-//                     result = parseFloat(n1) * parseFloat(n2);
-//                 } else if (operator === 'divide') {
-//                     result = parseFloat(n1) / parseFloat(n2)
-//                 }
-//                 console.log(result)
-//                 return result
-//             }
-//             display.textContent = calculate(firstValue, operator, secondValue)
-//         } if (action ==="clear"){
-//             display.textContent = "0"
-//             // console.log("byebye")
-//         }
-//         Array.from(key.parentNode.children)
-//             .forEach(k => k.classList.remove(".is-depressed"));
-        
-//         // calculate()
-
-//         //having it so we replace the placeholder display number with the number keys we hit
-//         // if(!action){
-//         //     if (displayedNum === '0'){
-//         //         display.textContent = keyContent;
-//         //     } else {
-//         //         display.textContent = displayedNum + keyContent;
-//         //     }
-//         // }
-//     }
-// });
 
 //1. on click of number keys. show that number in the display
 // const display = document.querySelector('.calc-display');
@@ -113,7 +24,7 @@ for (let i = 0; i < number.length ; i++){
         if(resultDisplayed === false){
             console.log("1")
             input.innerHTML += e.target.innerHTML;
-        } else if (resultDisplayed === true && lastChar === "+"  || lastChar === "-" || lastChar === "&times" || lastChar === "÷") {
+        } else if (resultDisplayed === true && lastChar === "+"  || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
         //if result is currently displayed and pressed a number
         //clear the input string and add the new input to start new operation
             console.log("2")
@@ -139,7 +50,7 @@ for(let i = 0; i < operator.length; i++){
         let lastChar = currentString[currentString.length - 1];
 
         //if last character entered is operator, replace with currently pressed one
-        if (lastChar === "+" || lastChar === "-" || lastChar === "&times" || lastChar === "÷") {
+        if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
             let newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML; 
             input.innerHTML = newString;
         } else if (currentString.length == 0){
@@ -148,5 +59,59 @@ for(let i = 0; i < operator.length; i++){
         } else {
             input.innerHTML += e.target.innerHTML
         }
-    })
+    });
 }
+
+//clicking 'equal' button
+result.addEventListener("click", function(){
+
+    let inputString = input.innerHTML;
+
+    //forming an array of numbers
+    //  /g is a global modifier. targets all instances .
+    let numbers = inputString.split(/\+|\-|\×|\÷/g);
+
+    //forming an array of operators
+    let operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+    console.log(inputString);
+    console.log(operators);
+    console.log(numbers);
+    console.log("lololololololo");
+
+    let divide = operators.indexOf("÷");
+    while(divide != -1){
+        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+        operators.splice(divide, 1);
+        divide = operators.indexOf("÷");
+    }
+
+    let multiply = operators.indexOf("×");
+    while (multiply != -1) {
+        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+        operators.splice(multiply, 1);
+        multiply = operators.indexOf("×");
+    }
+
+    let subtract = operators.indexOf("-");
+    while (subtract != -1){
+        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+        operators.splice(subtract, 1);
+        subtract = operators.indexOf("-");
+    }
+
+    let add = operators.indexOf("+");
+    while (add != -1) {
+        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+        operators.splice(add, 1);
+        add = operators.indexOf("+");
+    }
+
+    input.innerHTML = numbers[0];
+
+    resultDisplayed = true;
+
+    clear.addEventListener("click", function(){
+        input.innerHTML = "";
+    })
+})
